4a5036f80dc2d76518c847c5ad399f9a79edb4a8
Mehefuj Ali <mehefujali30@gmail.com>
2024-09-24 03:03:54 +0530

    history part modified

--- fitures.js
+++ fitures.js
@@ -1,17 +1,20 @@
 const donateBtn = document.getElementById('donate-btn')
 const historyBtn = document.getElementById('history-btn')
 const donationPart = document.getElementById('donation-part')
+const historySection = document.getElementById('history-section')
 
 historyBtn.addEventListener('click', function () {
       donateBtn.classList.remove('bg-primary-color')
       historyBtn.classList.add('bg-primary-color')
       donationPart.classList.add('hidden')
+      historySection.classList.remove('hidden')
 
 })
 donateBtn.addEventListener('click', function () {
       donateBtn.classList.add('bg-primary-color')
       historyBtn.classList.remove('bg-primary-color')
       donationPart.classList.remove('hidden')
+      historySection.classList.add('hidden')
 
 
 })
